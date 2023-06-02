88c7f14fb05578b2adb34d1201c1fe20616ec8d2
CB <clint@getkolla.com>
2023-06-01 19:09:23 -0600

    Transparent logo

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -55,13 +55,12 @@ export default function Index() {
                   </div>
                 )}
               </div>
-              <a href="https://remix.run">
-                <img
-                  src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
-                  alt="Remix"
-                  className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
-                />
-              </a>
+              <img
+                src="https://github.com/forgeutah/forge-website/blob/main/public/images/forge-icon.png?raw=true"
+                alt="Forge Utah Foundation"
+                className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
+              />
+
             </div>
           </div>
         </div>
